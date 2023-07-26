5a2c6bd6566f7e0e5b23f694817ff4b6c14581bb
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-07-26 16:59:33 -0300

    Added prediction plot

--- Stock Price Prediction.py
+++ Stock Price Prediction.py
@@ -226,3 +226,26 @@ plt.title('past and future stock prices')
 plt.xlabel('time [days]')
 plt.ylabel('normalized price')
 plt.legend(loc='best');
+
+
+plt.figure(figsize=(15, 5));
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test.shape[0]),
+         y_test[:,ft], color='black', label='test target')
+
+plt.plot(np.arange(y_train_pred.shape[0], y_train_pred.shape[0]+y_test_pred.shape[0]),
+         y_test_pred[:,ft], color='green', label='test prediction')
+
+plt.title('future stock prices')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price')
+plt.legend(loc='best');
+
+corr_price_development_train = np.sum(np.equal(np.sign(y_train[:,1]-y_train[:,0]),
+            np.sign(y_train_pred[:,1]-y_train_pred[:,0])).astype(int)) / y_train.shape[0]
+corr_price_development_valid = np.sum(np.equal(np.sign(y_valid[:,1]-y_valid[:,0]),
+            np.sign(y_valid_pred[:,1]-y_valid_pred[:,0])).astype(int)) / y_valid.shape[0]
+corr_price_development_test = np.sum(np.equal(np.sign(y_test[:,1]-y_test[:,0]),
+            np.sign(y_test_pred[:,1]-y_test_pred[:,0])).astype(int)) / y_test.shape[0]
+
+print('correct sign prediction for close - open price for train/valid/test: %.2f/%.2f/%.2f'%(
+    corr_price_development_train, corr_price_development_valid, corr_price_development_test))

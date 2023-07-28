a12cf08ccd5be2c588ecfda9d768437a1c4dbcfb
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-07-28 09:54:34 -0300

    Filtered IT sector stocks

--- stock_prediction_xgbregressor_model.py
+++ stock_prediction_xgbregressor_model.py
@@ -8,10 +8,13 @@ Created on Fri Jul 28 09:12:12 2023
 import numpy as np
 import pandas as pd
 
-df_technical = pd.read_csv(r"C:\Neha\kaggle Projects\Git hub\Finance\Dataset\prices-split-adjusted.csv")
-df_fundamental = pd.read_csv(r"C:\Neha\kaggle Projects\Git hub\Finance\Dataset\fundamentals.csv")
+df_technical = pd.read_csv(
+    r"C:\Neha\kaggle Projects\Git hub\Finance\Dataset\prices-split-adjusted.csv")
+df_fundamental = pd.read_csv(
+    r"C:\Neha\kaggle Projects\Git hub\Finance\Dataset\fundamentals.csv")
 df_fundamental = df_fundamental.drop("Unnamed: 0", axis=1)
-df_securities = pd.read_csv(r"C:\Neha\kaggle Projects\Git hub\Finance\Dataset\securities.csv")
+df_securities = pd.read_csv(
+    r"C:\Neha\kaggle Projects\Git hub\Finance\Dataset\securities.csv")
 
 
 print(df_technical)
@@ -25,3 +28,23 @@ df_fundamental.describe()
 print(df_securities)
 print(df_securities.info())
 df_securities.describe()
+
+
+df_securities["GICS Sector"].value_counts()
+
+# Filter IT sector stocks
+
+all_tech_symbol = list(df_securities[df_securities["GICS Sector"]=="Information Technology"]["Ticker symbol"])
+
+df_technical_tech = df_technical[df_technical["symbol"].isin(all_tech_symbol)]
+symbol_close = df_technical_tech.groupby("symbol")["volume"].mean().reset_index()
+symbol_close = symbol_close.sort_values(by="volume", ascending=False).reset_index(drop=True)[:3]
+top_tech_symbol = list(symbol_close["symbol"])
+
+print(df_securities[df_securities["Ticker symbol"].isin(top_tech_symbol)].reset_index(drop=True))
+
+df_technical = df_technical[df_technical["symbol"].isin(top_tech_symbol)].reset_index(drop=True)
+df_fundamental = df_fundamental[df_fundamental["Ticker Symbol"].isin(top_tech_symbol)].reset_index(drop=True)
+
+print(df_technical)
+print(df_fundamental)
